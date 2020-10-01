a5a2ef3790581744a283730b285917bd681ed381
codeaudio <64453492+codeaudio@users.noreply.github.com>
2020-10-01 09:13:04 +0300

    Update FacebookAuthorizTest.py

--- FacebookAuthorizTest.py
+++ FacebookAuthorizTest.py
@@ -7,7 +7,7 @@ from cryptography.fernet import Fernet
 link = "https://www.facebook.com/"                                                                                  #ссылка на тестируумую страницу
 
 data = {                                                                                                            #список с данными
-'email': '89031106963',                                                                                             #логин
+'email': 'nicehotgame@gmail.com',                                                                                             #логин
 'pass': b'gAAAAABfdDKhHAJG-J6XxjE4N-8ODSmwmU7FGZqvCPV8pjlbNUWvXad5Dg7K9mGX3AFlAGI4c2NoMumgQj3tO8Uaj77flRai_w=='     #зашифрованный пароль
 }
 cipher_key = b'HfASIcnseLe1xKEF244_yEqV_OM9R3tQO_P4ZR7bY00='                                                        #ключ для расшифровки
@@ -18,7 +18,7 @@ encrypted_text = data['pass']                                                   
 cipher = Fernet(cipher_key)
 decrypted_text = cipher.decrypt(encrypted_text)                                                                     #расшифровка пароля
 
-decode_pass = bytes.decode(decrypted_text, 'utf-8')                                                                 #перевод из bytes в str
+                                 
 
 try:
     options = Options()
@@ -37,7 +37,7 @@ try:
         email.send_keys(data['email'])                                                                               #ввод логина
 
         password = browser.find_element_by_id('pass')    
-        password.send_keys(decode_pass)                                                                              #ввод пароля
+        password.send_keys(bytes.decode(decrypted_text, 'utf-8'))                                                    #ввод пароля и перевод из bytes в str
 
         btn = browser.find_element_by_id('u_0_b')   
         btn.click()                                                                                                  #клик по кнопке вход
