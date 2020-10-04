5358cd9b1ea7b6908946effe8d5b3fca44cfee78
codeaudio <64453492+codeaudio@users.noreply.github.com>
2020-10-04 07:09:19 +0300

    Update FacebookAuthorizTest.py

--- FacebookAuthorizTest.py
+++ FacebookAuthorizTest.py
@@ -14,48 +14,50 @@ data = {                                                                        
 'email': 'nicehotgame@gmail.com',                                                                                   #логин
 'pass': b'gAAAAABfdDKhHAJG-J6XxjE4N-8ODSmwmU7FGZqvCPV8pjlbNUWvXad5Dg7K9mGX3AFlAGI4c2NoMumgQj3tO8Uaj77flRai_w=='     #зашифрованный пароль
 }
+  
+class TestAuth():
+    def test_1(self):
+        options = Options()
+        user_agent = fake_useragent.UserAgent() 
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'       #фейковый юзер агент
+        options.add_argument(f'user-agent={user_agent}')                                                                                         #добавление фейкового юзер агента
+        print(user_agent)
 
-options = Options()
-user_agent = fake_useragent.UserAgent() 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'       #фейковый юзер агент
-options.add_argument(f'user-agent={user_agent}')                                                                                         #добавление фейкового юзер агента
-print(user_agent)
+        browser = webdriver.Chrome(options=options)                                                                                       #добавление опции хрому
+        browser.implicitly_wait(5)
+        browser.get(link)  
 
-browser = webdriver.Chrome(chrome_options=options)                                                                                       #добавление опции хрому
-browser.implicitly_wait(5)
-browser.get(link)  
+        try:      
+            for cookie in pickle.load(open('session','rb')):                                                             #чтение файла куков
+                browser.add_cookie(cookie)
+            else:
+                browser.refresh()     
+                    
+        except FileNotFoundError:                                                                                        #если файла не существуетб то исполнить блок вводы данных       
+            cipher_key = b'HfASIcnseLe1xKEF244_yEqV_OM9R3tQO_P4ZR7bY00='                                                 #ключ для расшифровки
 
-try:          
-    for cookie in pickle.load(open('session','rb')):                                                             #чтение файла куков
-        browser.add_cookie(cookie)
+            encrypted_text = data['pass']                                                                                #строка парля из словаря
 
-except FileNotFoundError:                                                                                        #если файла не существуетб то исполнить блок вводы данных       
-    cipher_key = b'HfASIcnseLe1xKEF244_yEqV_OM9R3tQO_P4ZR7bY00='                                                 #ключ для расшифровки
+            cipher = Fernet(cipher_key)
+            decrypted_text = cipher.decrypt(encrypted_text)                                                              #расшифровка пароля
+                                        
+            email = browser.find_element_by_id('email')     
+            email.send_keys(data['email'])                                                                               #ввод логина
 
-    encrypted_text = data['pass']                                                                                #строка парля из словаря
+            password = browser.find_element_by_id('pass')    
+            password.send_keys(bytes.decode(decrypted_text, 'utf-8'))                                                    #ввод пароля и перевод из bytes в str
 
-    cipher = Fernet(cipher_key)
-    decrypted_text = cipher.decrypt(encrypted_text)                                                              #расшифровка пароля
-                                 
-    email = browser.find_element_by_id('email')     
-    email.send_keys(data['email'])                                                                               #ввод логина
+            btn =WebDriverWait(browser, 5).until(
+                EC.element_to_be_clickable((By.ID,'u_0_b'))
+                )
+            btn.click()                                                                                                  #клик по кнопке вход    
+            
+        finally:
+            time.sleep(1)
+            pickle.dump(browser.get_cookies(), open('session','wb'))                                                     #сохранение куков(сессии)
+            print('Вход в профиль')
+            browser.get_screenshot_as_file('screen.png')
+            browser.quit()
 
-    password = browser.find_element_by_id('pass')    
-    password.send_keys(bytes.decode(decrypted_text, 'utf-8'))                                                    #ввод пароля и перевод из bytes в str
-
-    btn =WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable((By.ID,'u_0_b'))
-        )
-    btn.click()                                                                                                  #клик по кнопке вход    
-     
-    pickle.dump(browser.get_cookies(), open('session','wb'))                                                     #сохранение куков(сессии)
-    print('Вход в профиль')
-
-else:        
-    browser.refresh()                                                                                            #перезагрузка при получении куков
-    print('Использование прошлой сессии')
-
-finally:
-    time.sleep(5)
-    browser.get_screenshot_as_file('screen.png')
-    browser.quit()
+if __name__ == "__main__":
+    TestAuth()
